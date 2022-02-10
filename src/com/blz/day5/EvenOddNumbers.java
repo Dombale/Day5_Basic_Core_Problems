ec3f8db0397fc97c9db49e7edd5a9c1a7803d8d2
Priyanka <priyankadombale27@gmail.com>
2022-02-10 23:20:59 +0530

    Added: Even Odd numbers problem Solution

--- src/com/blz/day5/EvenOddNumbers.java
+++ src/com/blz/day5/EvenOddNumbers.java
@@ -1,9 +1,23 @@
 package com.blz.day5;
 
+import java.util.Scanner;
+
 public class EvenOddNumbers {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome in Even-Odd Numbers...!!");
+		evenOddCheck();
+	}
+
+	static void evenOddCheck() {
+		System.out.println("The number is :");
+		Scanner sc = new Scanner(System.in);
+		int Num = sc.nextInt();
+
+		if (Num % 2 == 0)
+			System.out.println(Num + " number is Even");
+		else
+			System.out.println(Num + " number is Odd");
 
 	}
 
